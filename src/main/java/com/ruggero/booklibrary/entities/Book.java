96760ca70a75978eac786635b4cd219297f67c79
ruggeromontesi <ruggero.montesi@dematic.com>
2022-01-10 10:27:30 +0200

    improved formatting of  Book class

--- src/main/java/com/ruggero/booklibrary/entities/Book.java
+++ src/main/java/com/ruggero/booklibrary/entities/Book.java
@@ -1,323 +1,274 @@
 package com.ruggero.booklibrary.entities;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import  com.ruggero.booklibrary.service.Util;
 import java.util.UUID;
-
 import javax.persistence.Entity;
-
 import javax.persistence.Id;
+
+import com.ruggero.booklibrary.service.Util;
 
 @Entity
 public class Book {
-	static private int count = 0;
 
-	@Id
-	private  int id = count++;
-	private String name;
-	private String author;
-	private String category;
-	private String language;
-	private String publicationDate;
-	private String isbn;
-	private	String guid;
+   private static int count = 0;
 
-	
-	private boolean available;
-	private int bookedPeriodDays;
-	private String borrowerName;
+   @Id
+   private  int id = count++;
 
-	
-	 public Book(){
-		 guid = null;
-	 }
-	 
+   private String name;
 
-	public Book(String name, String author, String category, String language, String publicationDate, String isbn) {
-		
-		//setId();
-		//this.id=count++;
-		setGuid();
-		setName(name);
-		setAuthor(author);
-		setCategory(category);
-		setLanguage(language);
-		setPublicationDate(publicationDate);
-		setIsbn(isbn);
-		
-	}
-	
+   private String author;
 
-	
-	
+   private String category;
 
-	public int getId() {
-		return id;
-	}
+   private String language;
 
-	public void setId() {
-		//this.id=count++;
-		
-	}
+   private String publicationDate;
 
-	public String getName() {
-		return name;
-	}
+   private String isbn;
 
-	public void setName(String name) {
-		this.name = name;
-		setId();
-		setGuid();
-		setAvailable(true);
-		setBookedPeriodDays(-1);
-		setBorrowerName("");
-	}
+   private	String guid;
 
-	public String getAuthor() {
-		return author;
-	}
+   private boolean available;
 
-	public void setAuthor(String author) {
-		this.author = author;
-	}
+   private int bookedPeriodDays;
 
-	public String getCategory() {
-		return category;
-	}
+   private String borrowerName;
 
-	public void setCategory(String category) {
-		this.category = category;
-	}
+   public Book() {
+   }
 
-	public String getLanguage() {
-		return language;
-	}
+   public Book(String name, String author, String category, String language, String publicationDate, String isbn) {
+      setGuid();
+      this.name = name;
+      this.author = author;
+      this.category = category;
+      this.language = language;
+      this.publicationDate = publicationDate;
+      this.isbn = isbn;
+   }
 
-	public void setLanguage(String language) {
-		this.language = language;
-	}
+   public static int getCount() {
+      return count;
+   }
 
-	public String getPublicationDate() {
-		return publicationDate;
-	}
+   public static void setCount(int count) {
+      Book.count = count;
+   }
 
-	public void setPublicationDate(String publicationDateString) {
-		this.publicationDate = Util.validateStringToDate(publicationDateString);
-		/*LocalDate intDate;
-		DateTimeFormatter f;
-		try {
-		f = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-		intDate = LocalDate.parse(publicationDateString, f);
-		} catch(IllegalArgumentException e) {
-			System.out.println("Wrong date!!!");
-			this.publicationDate = null;
-			return;
-		} 
-		 if (intDate.compareTo(LocalDate.now()) > 0) {
-			System.out.println("Publication date next than current date!");
-			this.publicationDate = null;
-			return;
-			
-		}
-			
-		
-		this.publicationDate = intDate.format(f);*/
-	}
+   public int getId() {
+      return id;
+   }
 
-	public String getIsbn() {
-		return isbn;
-	}
+   public void setId(int id) {
+      this.id = id;
+   }
 
-	public void setIsbn(String isbn) {
-		
-		this.isbn = Util.validateIsbn(isbn);
-		
-		
-	}
+   public String getName() {
+      return name;
+   }
 
-	public String getGuid() {
-		return guid;
-	}
+   public void setName(String name) {
+      this.name = name;
+   }
 
-	public void setGuid() {
-		this.guid = UUID.randomUUID().toString();
-	}
+   public String getAuthor() {
+      return author;
+   }
 
-	public boolean getAvailable() {
-		return this.available;
-	}
+   public void setAuthor(String author) {
+      this.author = author;
+   }
 
-	public void setAvailable(boolean available) {
-		this.available = available;
-	}
+   public String getCategory() {
+      return category;
+   }
 
-	public int getBookedPeriodDays() {
-		return bookedPeriodDays;
-	}
+   public void setCategory(String category) {
+      this.category = category;
+   }
 
-	public void setBookedPeriodDays(int bookedPeriodDays) {
-		if (bookedPeriodDays >= -1 && bookedPeriodDays < 61)
-			this.bookedPeriodDays = bookedPeriodDays;
-		else
-			throw new IllegalArgumentException();
-	}
+   public String getLanguage() {
+      return language;
+   }
 
-	public String getBorrowerName() {
-		return borrowerName;
-	}
+   public void setLanguage(String language) {
+      this.language = language;
+   }
 
-	public void setBorrowerName(String borrowerName) {
-		this.borrowerName = borrowerName;
-		
-		}
-	
+   public String getPublicationDate() {
+      return publicationDate;
+   }
 
-	@Override
-	public String toString() {
-		return "Book [id=" + id + ", name=" + name + ", author=" + author + ", category=" + category + ", language="
-				+ language + ", publicationDate=" + publicationDate + ", ISBN=" + isbn + ", guid=" + guid + ", status="
-				+ available + ", bookedPeriod=" + bookedPeriodDays + ", borrowerName=" + borrowerName + "]";
-	}
+   public void setPublicationDate(String publicationDateString) {
+      this.publicationDate = Util.validateStringToDate(publicationDateString);
+   }
 
+   public String getIsbn() {
+      return isbn;
+   }
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((author == null) ? 0 : author.hashCode());
-		result = prime * result + ((category == null) ? 0 : category.hashCode());
-		result = prime * result + ((guid == null) ? 0 : guid.hashCode());
-		result = prime * result + ((isbn == null) ? 0 : isbn.hashCode()); 
-		result = prime * result + ((language == null) ? 0 : language.hashCode());
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
-		result = prime * result + ((publicationDate == null) ? 0 : publicationDate.hashCode());
-		return result;
-	}
+   public void setIsbn(String isbn) {
+      this.isbn = isbn;
+   }
 
+   public String getGuid() {
+      return guid;
+   }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj) {
-			return true;
-		}
-		if (!(obj instanceof Book)) {
-			return false;
-		}
-		Book other = (Book) obj;
-		if (author == null) {
-			if (other.author != null) {
-				return false;
-			}
-		} else if (!author.equals(other.author)) {
-			return false;
-		}
-		if (category == null) {
-			if (other.category != null) {
-				return false;
-			}
-		} else if (!category.equals(other.category)) {
-			return false;
-		}
-		if (guid == null) {
-			if (other.guid != null) {
-				return false;
-			}
-		} else if (!guid.equals(other.guid)) {
-			
-			return false;
-		}
-		
-		if (isbn == null) {
-			if(other.isbn != null) {
-			return false;
-			}
-		} else if (!isbn.equals(other.isbn)) {
-			return false;
-		}
-		
-		
-		if (language == null) {
-			if (other.language != null) {
-				return false;
-			}
-		} else if (!language.equals(other.language)) {
-			return false;
-		}
-		if (name == null) {
-			if (other.name != null) {
-				return false;
-			}
-		} else if (!name.equals(other.name)) {
-			return false;
-		}
-		if (publicationDate == null) {
-			if (other.publicationDate != null) {
-				return false;
-			}
-		} else if ( ! publicationDate.equals(other.publicationDate))  {
-			
-			System.out.println("different publication date!");
-			return false;
-		}
-		
-		return true;
-	}
-	
-	
-	public boolean looseEquals(Object obj) {
-		if (this == obj) {
-			return true;
-		}
-		if (!(obj instanceof Book)) {
-			return false;
-		}
-		Book other = (Book) obj;
-		if (author == null) {
-			if (other.author != null) {
-				return false;
-			}
-		} else if (!author.equals(other.author)) {
-			return false;
-		}
-		if (category == null) {
-			if (other.category != null) {
-				return false;
-			}
-		} else if (!category.equals(other.category)) {
-			return false;
-		}
-		
-		if (isbn != other.isbn) {
-			return false;
-		}
-		if (language == null) {
-			if (other.language != null) {
-				return false;
-			}
-		} else if (!language.equals(other.language)) {
-			return false;
-		}
-		if (name == null) {
-			if (other.name != null) {
-				return false;
-			}
-		} else if (!name.equals(other.name)) {
-			return false;
-		}
-		if (publicationDate == null) {
-			if (other.publicationDate != null) {
-				return false;
-			}
-		} else if ( ! publicationDate.equals(other.publicationDate))  {
-		
-			System.out.println("different publication date!");
-			return false;
-		}
-		return true;
-	}
-	
-	
+   private void setGuid() {
+      this.guid = UUID.randomUUID().toString();
+   }
 
+   public boolean getAvailable() {
+      return available;
+   }
 
+   public void setAvailable(boolean available) {
+      this.available = available;
+   }
+
+   public int getBookedPeriodDays() {
+      return bookedPeriodDays;
+   }
+
+   public void setBookedPeriodDays(int bookedPeriodDays) {
+      this.bookedPeriodDays = bookedPeriodDays;
+   }
+
+   public String getBorrowerName() {
+      return borrowerName;
+   }
+
+   public void setBorrowerName(String borrowerName) {
+      this.borrowerName = borrowerName;
+   }
+
+   @Override
+   public String toString() {
+      return "Book{"
+            + "id=" + id + '\''
+            + ", name='" + name + '\''
+            + ", author='" + author + '\''
+            + ", category='" + category + '\''
+            + ", language='" + language + '\''
+            + ", publicationDate='" + publicationDate + '\''
+            + ", isbn='" + isbn + '\''
+            + ", guid='" + guid + '\''
+            + ", available=" + available
+            + ", bookedPeriodDays=" + bookedPeriodDays
+            + ", borrowerName='" + borrowerName + '\''
+            + '}';
+   }
+
+   @Override
+   public boolean equals(Object o) {
+      if (this == o) {
+         return true;
+      }
+      if (!(o instanceof Book)) {
+         return false;
+      }
+
+      Book book = (Book) o;
+
+      if (getId() != book.getId()) {
+         return false;
+      }
+      if (getAvailable() != book.getAvailable()) {
+         return false;
+      }
+      if (getBookedPeriodDays() != book.getBookedPeriodDays()) {
+         return false;
+      }
+      if (!getName().equals(book.getName())) {
+         return false;
+      }
+      if (!getAuthor().equals(book.getAuthor())) {
+         return false;
+      }
+      if (!getCategory().equals(book.getCategory())) {
+         return false;
+      }
+      if (!getLanguage().equals(book.getLanguage())) {
+         return false;
+      }
+      if (!getPublicationDate().equals(book.getPublicationDate())) {
+         return false;
+      }
+      if (!getIsbn().equals(book.getIsbn())) {
+         return false;
+      }
+      if (!getGuid().equals(book.getGuid())) {
+         return false;
+      }
+      return getBorrowerName().equals(book.getBorrowerName());
+   }
+
+   @Override
+   public int hashCode() {
+      int result = getId();
+      result = 31 * result + getName().hashCode();
+      result = 31 * result + getAuthor().hashCode();
+      result = 31 * result + getCategory().hashCode();
+      result = 31 * result + getLanguage().hashCode();
+      result = 31 * result + getPublicationDate().hashCode();
+      result = 31 * result + getIsbn().hashCode();
+      result = 31 * result + getGuid().hashCode();
+      result = 31 * result + (getAvailable() ? 1 : 0);
+      result = 31 * result + getBookedPeriodDays();
+      result = 31 * result + getBorrowerName().hashCode();
+      return result;
+   }
+
+   public boolean looseEquals(Object obj) {
+      if (this == obj) {
+         return true;
+      }
+      if (!(obj instanceof Book)) {
+         return false;
+      }
+      Book other = (Book) obj;
+      if (author == null) {
+         if (other.author != null) {
+            return false;
+         }
+      } else if (!author.equals(other.author)) {
+         return false;
+      }
+      if (category == null) {
+         if (other.category != null) {
+            return false;
+         }
+      } else if (!category.equals(other.category)) {
+         return false;
+      }
+
+      if (isbn != other.isbn) {
+         return false;
+      }
+      if (language == null) {
+         if (other.language != null) {
+            return false;
+         }
+      } else if (!language.equals(other.language)) {
+         return false;
+      }
+      if (name == null) {
+         if (other.name != null) {
+            return false;
+         }
+      } else if (!name.equals(other.name)) {
+         return false;
+      }
+      if (publicationDate == null) {
+         if (other.publicationDate != null) {
+            return false;
+         }
+      } else if ( ! publicationDate.equals(other.publicationDate))  {
+
+         System.out.println("different publication date!");
+         return false;
+      }
+      return true;
+   }
 }
